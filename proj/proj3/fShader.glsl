0892eaefc559f14f24f08931032e20b9fd479968
Brian Park <bcpark@ncsu.edu>
2023-10-24 02:17:29 -0400

    fix color

--- proj/proj3/fShader.glsl
+++ proj/proj3/fShader.glsl
@@ -15,6 +15,5 @@ void main(void) {
     vec3 diffuse = diffuseScale * vDiffuse;
     float specularScale = pow(max(0.0, dot(vN, vH)), v_n);
     vec3 specular = specularScale * vSpecular;
-    vec4 color = vec4(vAmbient + diffuse + specular, 1.0);
-    gl_FragColor = color;
+    gl_FragColor = vec4(vAmbient + diffuse + specular, 1.0);
 }
