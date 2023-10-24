001ba8537928bafd393da0459804eb0b417d3e92
Brian Park <bcpark@ncsu.edu>
2023-10-24 02:25:10 -0400

    Fix lighting (#13)

--- proj/proj3/fShader.glsl
+++ proj/proj3/fShader.glsl
@@ -1,19 +1,19 @@
 precision highp float;
 
-varying vec4 vL;
-varying vec4 vN;
-varying vec4 vE;
-varying vec4 vAmbient;
-varying vec4 vDiffuse;
-varying vec4 vSpecular;
-varying vec4 vNormal;
+varying vec3 vL;
+varying vec3 vN;
+varying vec3 vE;
+varying vec3 vAmbient;
+varying vec3 vDiffuse;
+varying vec3 vSpecular;
+varying vec3 vNormal;
 varying float v_n;
 
 void main(void) {
-    vec4 vH = normalize(vL + vE);
+    vec3 vH = normalize(vL + vE);
     float diffuseScale = max(0.0, dot(vN, vL));
-    vec4 diffuse = diffuseScale * vDiffuse;
+    vec3 diffuse = diffuseScale * vDiffuse;
     float specularScale = pow(max(0.0, dot(vN, vH)), v_n);
-    vec4 specular = specularScale * vSpecular;
-    gl_FragColor = vAmbient + diffuse + specular;
+    vec3 specular = specularScale * vSpecular;
+    gl_FragColor = vec4(vAmbient + diffuse + specular, 1.0);
 }
